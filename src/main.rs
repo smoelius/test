0c2be214a9bb34c6a8279be0390e436487ecfaed
Samuel Moelius <sam@moeli.us>
2025-02-22 06:57:26 -0500

    More tests

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,22 @@ fn main() {
     if args().len() <= 1 {
         println!("{}", path.display());
         eprintln!("{}", path.display());
-    } else {
-        let assert = OutputAssert::new(Command::new(&path).output().unwrap());
-        assert
-            .stdout_eq(format!("{}\n", path.to_str().unwrap()))
-            .stderr_eq(format!("{}\n", path.to_str().unwrap()));
+        return;
     }
+
+    let arg = args().skip(1).next().unwrap();
+
+    if arg == "stdout" {
+        let assert = OutputAssert::new(Command::new(&path).output().unwrap());
+        assert.stdout_eq(format!("{}\n", path.to_str().unwrap()));
+        return;
+    }
+
+    if arg == "stderr" {
+        let assert = OutputAssert::new(Command::new(&path).output().unwrap());
+        assert.stderr_eq(format!("{}\n", path.to_str().unwrap()));
+        return;
+    }
+
+    panic!("expected `stdout` or `stderr`: {}", arg);
 }
